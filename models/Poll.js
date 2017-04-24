1121a5efcb0a05daf68285c0c363eb373fbee11c
hanmiton <hanmilton_12@outlook.com>
2017-04-24 03:06:01 -0500

    modificando modelo

--- models/Poll.js
+++ models/Poll.js
@@ -6,11 +6,20 @@ var voteSchema = new mongoose.Schema({ ip: 'String' });
 // Subdocument schema for poll choices
 var choiceSchema = new mongoose.Schema({ 
 	text: String,
+	desc: String,
 	votes: [voteSchema]
+});
+
+
+//subAsk
+var askSchema = new mongoose.Schema({ 
+	question: { type: String, required: true },
+	choices: [choiceSchema]
 });
 
 // Document schema for polls
 exports.PollSchema = new mongoose.Schema({
 	question: { type: String, required: true },
+	sub : askSchema,
 	choices: [choiceSchema]
 });
